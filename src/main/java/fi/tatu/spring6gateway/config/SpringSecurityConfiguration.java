cde64267685591b2cffb7da282edf5093b48c5e4
Tatu Soininen <taatusti@gmail.com>
2023-09-15 10:38:20 +0300

    Add OAuth 2.0 dependency + re-configure port mappings

---
+++ src/main/java/fi/tatu/spring6gateway/config/SpringSecurityConfiguration.java
@@ -0,0 +1,25 @@
+package fi.tatu.spring6gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.reactive.config.EnableWebFlux;
+
+@Configuration
+@EnableWebFlux
+public class SpringSecurityConfiguration {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http.csrf().disable()
+            .authorizeExchange()
+            .anyExchange()
+            .authenticated()
+            .and()
+            .oauth2ResourceServer()
+            .jwt();
+
+        return http.build();
+    }
+}
